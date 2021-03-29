48bec67c5f61fe26ba552e8487dc03ca38a54c67
Lam Nguyen <lamnn@imt-soft.com>
2021-03-29 22:05:41 +0700

    - update test type

--- app/controllers/admin/test_types_controller.rb
+++ app/controllers/admin/test_types_controller.rb
@@ -4,18 +4,40 @@ class Admin::TestTypesController < ApplicationController
   before_action :is_signed_in?
 
   def index
-    @testTypes = TestType.all
+    @test_types = TestType.all
+    @breadcrumbs = [["Test Type", admin_test_types_url]]
   end
 
   def new
-    @testType = TestType
+    @test_type = TestType.new
+    @breadcrumbs = [["Test Type", admin_test_types_url], ["New Test Type", new_admin_test_type_url]]
   end
 
   def create
-    testType = TestType.new
-    testType.en_name = params[:en_name]
-    testType.vi_name = params[:vi_name]
-    testType.save
+    @test_type = TestType.new(test_type_params)
+    @test_type.save
     redirect_to  admin_test_types_path
   end
+
+  def edit
+    @test_type = TestType.find(params[:id])
+    @breadcrumbs = [["Test Type", admin_test_types_url], ["Edit Test Type", edit_admin_test_type_url(params[:id])]]
+  end
+
+  def update
+    @test_type = TestType.find(params[:id])
+    @test_type.update(test_type_params)
+    redirect_to  admin_test_types_path
+  end
+
+  def destroy
+    @test_type = TestType.find(params[:id])
+    @test_type.delete
+    redirect_to  admin_test_types_path
+  end
+
+  private
+    def test_type_params
+      params.require(:test_type).permit(:en_name, :vi_name)
+    end
 end
