ceed0835fdfff90285848459b7128fc5b2f20ddd
Lam Nguyen <sogoten6689@gmail.com>
2021-04-02 01:09:17 +0700

    - update adminlte

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,14 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require('admin-lte');
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-// require('admin-lte');
 
-import 'bootstrap';
+// import 'bootstrap';
 import '../stylesheets/application.scss';
 import "@fortawesome/fontawesome-free/js/all";
 
