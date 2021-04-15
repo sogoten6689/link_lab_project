facf5cf33eaf5c14d20413be1934a78b8dda845a
Lam Nguyen <lamnn@imt-soft.com>
2021-04-15 10:22:34 +0700

    - cập nhật gói xét nghiệm và xét nghiệm

--- app/models/test_type.rb
+++ app/models/test_type.rb
@@ -1,7 +1,7 @@
 class TestType < ApplicationRecord
   has_many :raw_test
 
-  validates :en_name, presence: true, uniqueness: true
+  # validates :en_name, presence: true, uniqueness: true
   validates :vi_name, presence: true, uniqueness: true
 
 end
