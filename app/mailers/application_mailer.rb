08d20c1a0f85987e2464b52b3ac8871d3247f02d
Lam Nguyen <sogoten6689@gmail.com>
2021-07-17 19:07:03 +0700

    - fix bug

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,8 +3,14 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
 
   def send_password(user, password)
-    @user = user
-    @password = password
-    mail(to: @user.email, subject: "Welcome to LinkLab!")
+
+    @hello = "Hello " + user.username + "!"
+    @welcome = "Welcome to LinkLab, please use your below credentials to login:"
+    @account = "Your account: " + user.email
+    @password = "Your password: " + password
+
+    @user_session_url = user_session_url
+    @login_text = t('login')
+    mail(to: user.email, subject: "Welcome to LinkLab!")
   end
 end
