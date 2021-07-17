4f2352627fa9b88a08de9915492d50cd2803e921
Lam Nguyen <sogoten6689@gmail.com>
2021-07-17 18:38:33 +0700

    - fix text

--- app/mailers/register_success_mailer.rb
+++ app/mailers/register_success_mailer.rb
@@ -5,6 +5,8 @@ class RegisterSuccessMailer < ApplicationMailer
     @content += "Welcome to LinkLab, please use your below credentials to login:"
     @content += "Your account: " + @user.email
     @content += "Your password: " + password
+    @user_session_url = user_session_url
+    @login_text = t('login')
     mail(to: @user.email, subject: "LinkLab Email")
   end
 end
