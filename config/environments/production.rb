552224025f84b9cd5a094c0644b217cc99737812
Lam Nguyen <sogoten6689@gmail.com>
2021-07-17 19:44:14 +0700

    - update mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,12 +121,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "https://linklab.herokuapp.com/" }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
-    authentication: :plain,
-    enable_starttls_auto: true
+    authentication: "plain",
+    enable_starttls_auto: true,
   }
 end
