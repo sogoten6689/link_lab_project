84b88e3012529eb2a4ddac627417b5c895ad40aa
Lam Nguyen <sogoten6689@gmail.com>
2021-07-18 18:44:37 +0700

    - update js, css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,34 +2,27 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-require('admin-lte');
-global.toastr = require("toastr")
+require('admin-lte')
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
-import 'bootstrap';
-import '../stylesheets/application.scss';
-import "@fortawesome/fontawesome-free/js/all";
-
-document.addEventListener("turbolinks:load", () => {
-    $('[data-toggle="tooltip"]').tooltip()
-});
+import 'bootstrap'
+import '../stylesheets/application.scss'
+import "@fortawesome/fontawesome-free/css/all.css";
+import 'select2'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+global.toastr = require("toastr")
+
 window.jQuery = $;
 window.$ = $;
-
-import 'select2'
-import 'select2/dist/css/select2.css'
-
-window.addEventListener('DOMContentLoaded', () => {
-    $('.select2').select2();
-})
-
